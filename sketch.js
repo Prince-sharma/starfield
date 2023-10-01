0496194d5901947726e0597993c49394452863ca
prince-sharma <princesharma.pri@gmail.com>
2023-10-01 14:49:14 +0530

    added V1 of code

---
+++ sketch.js
@@ -0,0 +1,25 @@
+let stars = [];
+let radius = 4;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 2500; i++) {
+    stars[i] = new star(
+      random(-width / 2, width / 2),
+      random(-height / 2, height / 2),
+      random(width),
+      radius
+    );
+  }
+}
+
+function draw() {
+  background(0);
+  let speedS = map(mouseX, 0, width, 0, 50);
+  translate(width / 2, height / 2);
+  for (s of stars) {
+    s.update(speedS);
+    s.show();
+  }
+}
